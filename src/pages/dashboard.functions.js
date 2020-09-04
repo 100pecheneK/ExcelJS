9ef97f2e76fee7c4199bd51dfc681ab2c417519c
100pecheneK <mistermihail23@gmail.com>
2020-09-04 11:40:34 +0500

    Finish routing

---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,53 @@
+import {storage} from '@core/utils'
+
+
+function getAllKeys() {
+  const keys = []
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+    if (key.includes('excel')) {
+      keys.push(key)
+    }
+  }
+  return keys
+}
+
+
+function toHTML({headerTitle, updated, id}) {
+  return `
+      <li class="dashboard__table__list__record">
+          <a href="#excel/${id}">${headerTitle}</a>
+          <strong>
+            ${new Date(updated).toLocaleDateString()}
+            ${new Date(updated).toLocaleTimeString()}
+          </strong>
+      </li>
+  `
+}
+
+
+export function createRecordsTable() {
+  const keys = getAllKeys()
+  if (!keys.length) {
+    return `<p>Таблиц нет</p>`
+  }
+  let records = []
+  keys.forEach((key) => {
+    records.push({...storage(key), id: key.split(':')[1]})
+  })
+  records = records.sort(function (a, b) {
+    let c = new Date(a.updated)
+    let d = new Date(b.updated)
+    return d - c
+  })
+  return `
+    <div class="dashboard__table__header">
+        <span>Название</span>
+        <span>Дата открытия</span>
+    </div>
+    <ul class="dashboard__table__list">
+        ${records.map(toHTML).join('')}
+    </ul>
+    `
+}
+
