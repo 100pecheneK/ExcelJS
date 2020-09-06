1ddbfdcb8026f7af4b3059de0853ab9ad044d140
100pecheneK <mistermihail23@gmail.com>
2020-09-06 20:23:20 +0500

    Add test for createStore.js and Router.js

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,46 @@
+import {Router} from '@core/routes/Router'
+import {Page} from '@core/Page'
+
+
+class Dashboard extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'Dashboard'
+    return root
+  }
+}
+
+class Excel extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'Excel'
+    return root
+  }
+}
+
+describe('Router:', () => {
+  let router
+  let $root
+  beforeEach(() => {
+    $root = document.createElement('div')
+    router = new Router($root, {
+      Dashboard,
+      Excel
+    })
+  })
+
+  test('should be defined', () => {
+    expect(router).toBeDefined()
+  })
+
+  test('should render Dashboard Page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>Dashboard</div>')
+  })
+
+  test('should render Excel Page', () => {
+    window.location.hash = 'excel'
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>Excel</div>')
+  })
+})
